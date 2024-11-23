7112ed553fff2259671d626bf000f74b9de34770
MichaelJoep <michaelp4great@gmail.comm>
2024-11-23 18:20:54 +0100

    vercel, pusher added

--- app_api/utils/websocketEvents.js
+++ app_api/utils/websocketEvents.js
@@ -1,3 +1,15 @@
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.SECRET_KEY,
+  secret: process.env.MERCHANT_PASSWORD,
+  cluster: "us2", 
+  useTLS: true,
+});
+
+
+
 module.exports = (io) => {
     io.on("connection", (socket) => {
         console.log(`New client connected: ${socket.id}`);
@@ -6,14 +18,14 @@ module.exports = (io) => {
         socket.on("joinRoom", (room) => {
             socket.join(room);
             console.log(`Client ${socket.id} joined room: ${room}`);
-            socket.to(room).emit("userJoined", `${socket.id} joined the room.`);
+            pusher.trigger(room, "userJoined", { message: `${socket.id} joined the room.` });
         });
   
       // Handle in-game events (e.g., player movements, attacks, kills)
     socket.on("gameEvent", (data) => {
         console.log("Game Event Received:", data);
         // Broadcast game updates to other players in the same room
-        socket.to(data.room).emit("gameUpdate", data);
+        pusher.trigger(data.room, "gameUpdate", data);
     });
       // Disconnect handler
       socket.on("disconnect", () => {
