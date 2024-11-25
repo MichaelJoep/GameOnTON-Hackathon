86e00f24d41ae46269750dde75c8d6f613ad8566
MichaelJoep <michaelp4great@gmail.comm>
2024-11-25 13:11:26 +0100

    vercel updated

--- frontend/src/pages/store/Store.jsx
+++ frontend/src/pages/store/Store.jsx
@@ -9,8 +9,8 @@ import "./Store.css";
 
 // Mock Data for Weapons
 const weapons = [
-  { id: 1, name: 'Blue Archive Weapon', price: 3000, modelPath: "/models/weapons/blue_weapon.glb", scale: [12, 12, 12] },
-  { id: 2, name: 'Assault Riffle', price: 1500, modelPath: "/models/weapons/Assault Riffle.glb", scale: [1.9, 1.9, 1.9] },
+  { id: 1, name: 'Blue Archive Weapon', price: 3000, modelPath: "/models/weapons/blue_weapon.glb", scale: [8, 8, 8] },
+  { id: 2, name: 'Assault Riffle', price: 1500, modelPath: "/models/weapons/AssaultRifle.glb", scale: [1.9, 1.9, 1.9] },
   { id: 3, name: 'Knife', price: 30000, modelPath: "/models/weapons/knife.glb", scale: [3.1, 3.1, 3.1] },
   { id: 4, name: 'Pistol', price: 20000, modelPath: "/models/weapons/Pistol.glb", scale: [8.7, 8.7, 8.7] }
 ];
